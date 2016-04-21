f33b299b230fec85d5a5eefcb5fe74d70024b3f2
zypz1990 <zypz1990@163.com>
2016-04-21 15:47:37 +0800

    add user following

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,18 +1,47 @@
 class User < ActiveRecord::Base
+	 attr_accessor :remember_token
 	 has_many :microposts, dependent: :destroy
+	 has_many :active_relationships, class_name:  "Relationship",
+                                  foreign_key: "follower_id",
+                                  dependent:   :destroy
+     has_many :following, through: :active_relationships, source: :followed
+      
+      has_many :passive_relationships, class_name:  "Relationship",
+                                   foreign_key: "followed_id",
+                                   dependent:   :destroy
+      has_many :followers, through: :passive_relationships, source: :follower               
 
-	attr_accessor :remember_token
+	
 	 before_save { self.email = email.downcase }
-  validates :name,  presence: true, length: { maximum: 50 }
-  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-  validates :email, presence: true, length: { maximum: 255 },
-                    format: { with: VALID_EMAIL_REGEX },
-                    uniqueness: { case_sensitive: false }
+	 validates :name,  presence: true, length: { maximum: 50 }
+	 VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+	 validates :email, presence: true, length: { maximum: 255 },
+	                    format: { with: VALID_EMAIL_REGEX },
+                        uniqueness: { case_sensitive: false }
   
+	 has_secure_password #has_secure_password 方法本身会验证存在性，但是可惜，只会验证有没有密码，因此用户可以创建 “ ”（6 个空格）这样的无效密码
+	 validates :password, presence: true, length: { minimum: 6 },allow_nil:true
 
+ # 返回用户的动态流
+  def feed
+   following_ids = "SELECT followed_id FROM relationships
+                     WHERE  follower_id = :user_id"
+    Micropost.where("user_id IN (#{following_ids}) OR user_id = :user_id", user_id: id)
+  end
+# 关注另一个用户
+  def follow(other_user)
+    active_relationships.create(followed_id: other_user.id)
+  end
 
- has_secure_password #has_secure_password 方法本身会验证存在性，但是可惜，只会验证有没有密码，因此用户可以创建 “ ”（6 个空格）这样的无效密码
- validates :password, presence: true, length: { minimum: 6 },allow_nil:true
+  # 取消关注另一个用户
+  def unfollow(other_user)
+    active_relationships.find_by(followed_id: other_user.id).destroy
+  end
+
+  # 如果当前用户关注了指定的用户，返回 true
+  def following?(other_user)
+    following.include?(other_user)
+  end
 
  # 返回指定字符串的哈希摘要
   def User.digest(string)
@@ -42,9 +71,9 @@ class User < ActiveRecord::Base
     update_attribute(:remember_digest, nil)
   end
 
-# 实现动态流原型
-  def feed
-    Micropost.where("user_id = ?", id)
-  end
+# # 实现动态流原型
+#   def feed
+#     Micropost.where("user_id = ?", id)
+#   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -13,8 +13,15 @@ Rails.application.routes.draw do
 
   get 'about' => 'static_pages#about'
 
-  resources :users
+   resources :users do
+    member do
+      get :following, :followers
+    end
+  end
+
   resources :microposts,          only: [:create, :destroy]
+    resources :relationships,       only: [:create, :destroy]
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
